b6f431718b45017cb09a0cb921b5c35c40f19530
Mohamed Magdy <89040973+MohamedMagdy097@users.noreply.github.com>
2024-01-27 11:10:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-// import fs from "fs";
+// import path from 'path';
+// // import fs from "fs";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(cors());
 const port = 3000;
 
-app.get('/pls', (req, res) => {
-  // Specify the path to your JSON file
-  const filePath = path.join(__dirname, 'shit', 'msg.json');
+// app.get('/pls', (req, res) => {
+//   // Specify the path to your JSON file
+//   const filePath = path.join(__dirname, 'shit', 'msg.json');
 
-  // Send the JSON file in the response
-  res.sendFile(filePath);
-});
+//   // Send the JSON file in the response
+//   res.sendFile(filePath);
+// });
 app.post('/this', (req, res) => {
   const msg = req.body;
   res.send(msg);
