72f601f95b09ef9e8d05b0ce540c62fd252eedff
Mohamed Magdy <89040973+MohamedMagdy097@users.noreply.github.com>
2024-01-27 14:28:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-// import path from 'path';
-// // import fs from "fs";
+import fs from "fs";
+import path from 'path';
+import { join } from 'path';
+
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -9,18 +11,36 @@ app.use(express.json());
 app.use(cors());
 const port = 3000;
 
-// app.get('/pls', (req, res) => {
-//   // Specify the path to your JSON file
-//   const filePath = path.join(__dirname, 'shit', 'msg.json');
 
-//   // Send the JSON file in the response
-//   res.sendFile(filePath);
-// });
+app.get('/getJsonFile', (req, res) => {
+  // Specify the path to your JSON file
+  const filePath = join(process.cwd(), 'shit', 'msg.json');
+
+  // Read the file using fs
+  fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+          return;
+      }
+
+      // Parse the JSON data
+      const jsonData = JSON.parse(data);
+
+      // Send the JSON data in the response
+      res.json(jsonData);
+  });
+});
+
+app.get('/pls', (req, res) => {
+  // Specify the path to your JSON file
+  const filePath = fs.readFile('./shit/msg.json')
+
+  // Send the JSON file in the response
+  res.sendFile(filePath);
+});
 app.post('/this', (req, res) => {
-  console.log("this: ");
-  console.log(req.body);
-  const msg = req.body.msg;
-  console.log(msg);
+  const msg = JSON.parse(req.body);
   res.send(msg);
 });
 
