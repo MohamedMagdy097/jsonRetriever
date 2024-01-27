41eb7be7f096d14d8425b27b561052ffbef4cf64
Mohamed Magdy <zalatanback5@gmail.com>
2024-01-27 09:54:02 +0200

    initial

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require("cors");
+const path = require('path');
+// import fs from "fs";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+
+app.get('/', (req, res) => {
+  // Specify the path to your JSON file
+  const filePath = path.join(__dirname, 'shit', 'msg.json');
+
+  // Send the JSON file in the response
+  res.sendFile(filePath);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
